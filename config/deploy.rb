98a72d88597a7aaec078dd3b1428d5259f6046bf
prasad <durgaprasad.b@nyros.in>
2011-05-05 15:42:22 +0530

    modified files

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_to,  "/media/disk-1/ror_projects/prasad/#{application}"
 role :web, "172.20.1.33"                          # Your HTTP server, Apache/etc
 role :app, "172.20.1.33"                          # This may be the same as your `Web` server
 role :db,  "172.20.1.33", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+#~ role :db,  "your slave db-server here"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
