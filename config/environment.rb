d045561c21b511b25bcc9802ca27b1a5c00494a8
prasad <durgaprasad.b@nyros.in>
2011-05-20 15:37:59 +0530

    sqlite added

--- config/environment.rb
+++ config/environment.rb
@@ -38,6 +38,8 @@ Rails::Initializer.run do |config|
   
   config.gem 'koala'
   config.gem 'fb_graph'
+  
+  gem 'sqlite3-ruby', :require => 'sqlite3', :version => '2.2.3'
 	
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
