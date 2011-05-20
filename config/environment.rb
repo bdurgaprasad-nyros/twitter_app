13dcef8bd30bd3ed7a07a16ba680e270fa8c1864
prasad <durgaprasad.b@nyros.in>
2011-05-20 16:11:28 +0530

    add sqlite gem

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,14 @@
-# Be sure to restart your server when you modify this file
+	# Be sure to restart your server when you modify this file
 
-# Uncomment below to force Rails into production mode when
-# you don't control web/app server and can't set it the proper way
-# ENV['RAILS_ENV'] ||= 'production'
+	# Uncomment below to force Rails into production mode when
+	# you don't control web/app server and can't set it the proper way
+	# ENV['RAILS_ENV'] ||= 'production'
 
-# Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+	# Specifies gem version of Rails to use when vendor/rails is not present
+	RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
-# Bootstrap the Rails environment, frameworks, and default configuration
-require File.join(File.dirname(__FILE__), 'boot')
+	# Bootstrap the Rails environment, frameworks, and default configuration
+	require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
@@ -29,17 +29,13 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
 
   #config.gem 'oauth', :version => '0.3.6'
-   config.gem 'rails', :version => '2.3.8'
-  
+  config.gem 'rails', :version => '2.3.8'  
   config.gem 'oauth', :version => '0.4.0'
   config.gem 'capistrano'
-  config.gem 'heroku'
-  
-  
+  config.gem 'heroku'  
   config.gem 'koala'
-  config.gem 'fb_graph'
-  
-  gem 'sqlite3-ruby', :require => 'sqlite3', :version => '2.2.3'
+  config.gem 'fb_graph'  
+  config.gem 'sqlite-ruby'
 	
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
@@ -86,13 +82,13 @@ Rails::Initializer.run do |config|
   # Please note that observers generated using script/generate observer need to have an _observer suffix
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
   
-  
-	# Twitter OAuth settings
-	TWOAUTH_SITE = 'http://twitter.com'
-	# Twitter OAuth callback default
-	TWOAUTH_CALLBACK = 'http://172.20.1.33:4000/members/callback'
-	# Twitter OAuth Consumer key
-	TWOAUTH_KEY = 'b8CSyOhjzZRywEeqzS3rg'
-	# Twitter OAuth Consumer secret
-	TWOAUTH_SECRET ='KrFh5z1KTlN6bZx18wzoesrQwVYiMUnzLLIOdmbBYWo'
+
+  # Twitter OAuth settings
+  TWOAUTH_SITE = 'http://twitter.com'
+  # Twitter OAuth callback default
+  TWOAUTH_CALLBACK = 'http://172.20.1.33:4000/members/callback'
+  # Twitter OAuth Consumer key
+  TWOAUTH_KEY = 'b8CSyOhjzZRywEeqzS3rg'
+  # Twitter OAuth Consumer secret
+  TWOAUTH_SECRET ='KrFh5z1KTlN6bZx18wzoesrQwVYiMUnzLLIOdmbBYWo'
 end
