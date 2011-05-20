809a57bc58df0124f238b11c87a4600d3cd0e8fd
prasad <durgaprasad.b@nyros.in>
2011-05-20 17:28:10 +0530

    added freezed zems

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,14 @@
-	# Be sure to restart your server when you modify this file
+  # Be sure to restart your server when you modify this file
 
-	# Uncomment below to force Rails into production mode when
-	# you don't control web/app server and can't set it the proper way
-	# ENV['RAILS_ENV'] ||= 'production'
+  # Uncomment below to force Rails into production mode when
+  # you don't control web/app server and can't set it the proper way
+  # ENV['RAILS_ENV'] ||= 'production'
 
-	# Specifies gem version of Rails to use when vendor/rails is not present
-	RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+  # Specifies gem version of Rails to use when vendor/rails is not present
+  RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
-	# Bootstrap the Rails environment, frameworks, and default configuration
-	require File.join(File.dirname(__FILE__), 'boot')
+  # Bootstrap the Rails environment, frameworks, and default configuration
+  require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
@@ -29,7 +29,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
 
   #config.gem 'oauth', :version => '0.3.6'
- 
+  config.gem 'rails', :version => '2.3.8'  
   config.gem 'oauth', :version => '0.4.0'
   config.gem 'capistrano'
   config.gem 'heroku'  
